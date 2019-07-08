74bbea4362d79d516c8f85688a111d07600da4f5
bhrt-sharma <bharatswaroop_16374@aitpune.edu.in>
2019-07-08 11:57:00 +0530

    le

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ import os
 from flask import Flask, flash, request, redirect, render_template,  jsonify
 from sklearn.externals import joblib
 from flask_material import Material
-
+import numpy as np
 from sklearn import metrics
 import pandas as pd
+
+# from sklearn.externals import joblib
+
+# And now to load...
+
+
+scaler_filename = "scaler.save"
+scaler = joblib.load(scaler_filename) 
+
 
 app = Flask(__name__)
 
@@ -20,37 +29,43 @@ def idk():
 def analyze():
 	# Retreive query parameters related to this request.
 	status = {
-		'eCode': None
+		'eCode': None,
+		'll': None
 	}
-	print(request.method)
 	if request.method == 'POST':
-		print(request.form)
-		user_id = request.form["user_id"].astype(np.int)
-		location_browser_lang = request.form["location_browser_lang"].astype(np.int)
-		config_browser_engine = request.form["config_browser_engine"].astype(np.int)
-		config_browser_name = request.form["config_browser_name"].astype(np.int)
-		config_device_brand=request.form['config_device_brand'].astype(np.int)
-		config_device_model=request.form['config_device_model'].astype(np.int)
-		config_os=request.form['config_os'].astype(np.int)
-		config_os_version=request.form['config_os_version'].astype(np.int)
-		config_resolution=request.form['config_resolution'].astype(np.int)
-		location_city=request.form['location_city'].astype(np.int)
-		location_country=request.form['location_country'].astype(np.int)
-		location_longitude=request.form['location_longitude'].astype(np.int)
+	
+		user_id = int(request.form["user_id"])
+		location_browser_lang = int(request.form["location_browser_lang"])
+		config_browser_engine = int(request.form["config_browser_engine"])
+		config_browser_name = int(request.form["config_browser_name"])
+		config_device_brand= int(request.form['config_device_brand'])
+		config_device_model= int(request.form['config_device_model'])
+		config_os= int(request.form['config_os'])
+		config_os_version= int(request.form['config_os_version'])
+		config_resolution= int(request.form['config_resolution'])
+		location_city= int(request.form['location_city'])
+		location_country= int(request.form['location_country'])
+		location_longitude= int(request.form['location_longitude'])
 
-		# features = [[user_id,location_browser_lang,config_browser_engine,config_browser_name,config_device_brand,config_device_model, config_os , config_os_version,config_resolution,location_city,location_country,location_longitude]]
-		
-		# features = pd.DataFrame(features)
+		features = [user_id, location_browser_lang, config_browser_engine, config_browser_name, 
+		config_device_brand, config_device_model, config_os , config_os_version, 
+		config_resolution, location_city, location_country, location_longitude]
 
-		# label_index = MODEL.predict(features)
-		# print(label_index)
-		# if (label_index == -1):
 
-		# 	label = "Bad user"
-		# else:
-		# 	label= "Good user"
+		features = np.array(features)
+		features = scaler.transform(features)		
+		label_index = MODEL.predict(features)
+		# print(features)
+		if (label_index == -1):
+			label = "Bad user"
+		else:
+			label= "Good user"
 		status['eCode'] = "Inside"	
+	
+		status['ll'] = label
+
 		return jsonify(status)
+
 
 	return jsonify(status)
 
